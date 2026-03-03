98ab94414b2d27984d2c5c5dd03ff5b00a7047a4
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-03-03 13:26:31 +0600

    update

--- resources/views/home-one.blade.php
+++ resources/views/home-one.blade.php
@@ -16,7 +16,7 @@
 
         .sec-style-1 .sec-body .sec-grid-box {
             display: grid;
-            grid-template-columns: repeat(3, minmax(0, 1fr));
+            grid-template-columns: repeat(4, minmax(0, 1fr));
             gap: 20px;
         }
 
