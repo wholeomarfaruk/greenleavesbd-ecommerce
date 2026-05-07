155ca1bc212ce0a6f6b82abefc15b23bd10ffe81
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-05-07 08:45:43 +0600

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Green Leaves BD</title>
+    <title>Green Leaves Bangladesh</title>
     <link rel="icon" type="image/png" href="{{ asset('frontend/img/logo-transparent.png') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css"
         integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ=="
